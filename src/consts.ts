5e945141160ec103b52d4030ee0ab847c08a3965
Zach Bochanski <ztbochanski@gmail.com>
2024-01-24 12:43:59 -0800

    add descriptions links and animation class

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,17 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+export const SITE_TITLE = 'Zach - Software Engineer';
+export const SITE_DESCRIPTION = 'Software engineer portfolio with strong focus on software engineering fundamentals, systems, and data science.';
+export const BLOG_TITLE = 'Zach - Blog';
+export const BLOG_DESCRIPTION = 'Notes on software engineering, data science, and systems.';
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const LINKS = {
+  github: "https://github.com/ztbochanski",
+  linkedin: "https://www.linkedin.com/in/zach-bochanski-7a4710111/",
+  discord: "https://discordapp.com/users/746101582650802286",
+  rickroll: "https://youtu.be/rTga41r3a4s?t=18",
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
