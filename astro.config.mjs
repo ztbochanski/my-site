09648d110f31b9915a7d8904a4ebca01b5d7a413
Zach Bochanski <ztbochanski@gmail.com>
2024-01-24 14:50:32 -0800

    add remark reading time config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import { remarkReadingTime } from './src/remark-reading-time.mjs';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,7 +11,6 @@ export default defineConfig({
 	integrations: [mdx({
 		syntaxHighlight: 'shiki',
 		shikiConfig: { theme: 'nord' },
-		remarkPlugins: [],
-
+		remarkPlugins: [remarkReadingTime],
 	}), sitemap()],
 });
