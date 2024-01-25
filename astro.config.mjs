2b9d7173c894ccccc9aa74a65aa0e067bb3247c5
Zach Bochanski <ztbochanski@gmail.com>
2024-01-25 14:21:17 -0800

    add tailwind package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from 'astro/config';
 import { remarkReadingTime } from './src/remark-reading-time.mjs';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx({
-		syntaxHighlight: 'shiki',
-		shikiConfig: { theme: 'nord' },
-		remarkPlugins: [remarkReadingTime],
-	}), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx({
+    remarkPlugins: [remarkReadingTime]
+  }), sitemap(), tailwind(), icon()]
 });
