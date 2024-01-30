cdbad314b8ebbb592d1901a59d9d95a193156781
Zach Bochanski <ztbochanski@gmail.com>
2024-01-30 00:07:10 -0800

    add themes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,6 @@ export default {
 	},
 	plugins: [require('@tailwindcss/typography'), require('daisyui')],
 	daisyui: {
-		themes: ["light", "dark"]
+		themes: ["light", "nord", "dark", "black", "acid"]
 	},
 }
