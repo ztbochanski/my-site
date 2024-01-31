95bd0f750effa8bfbc4a713f302f2ea8700086d9
Zach Bochanski <ztbochanski@gmail.com>
2024-01-31 15:47:47 -0800

    add site link

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://zachsblog.netlify.app',
   integrations: [mdx({
     remarkPlugins: [remarkReadingTime]
   }), sitemap(), tailwind(), icon()]
