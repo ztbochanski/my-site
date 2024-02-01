491bc7dd966f937e5afcc81cfe480d585569d229
Zach Bochanski <ztbochanski@gmail.com>
2024-02-01 10:27:14 -0800

    add compression utility

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import { remarkReadingTime } from './src/remark-reading-time.mjs';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zachsblog.netlify.app',
   integrations: [mdx({
     remarkPlugins: [remarkReadingTime]
-  }), sitemap(), tailwind(), icon()],
+  }), sitemap(), tailwind(), icon(), compress()]
 });
