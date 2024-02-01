69a5c8bd25360d0e7e08f81f9bf14e23309bf671
Zach Bochanski <ztbochanski@gmail.com>
2024-02-01 15:27:12 -0800

    remove astro compress
    probing for failed build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,10 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
-import compress from "astro-compress";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zachsblog.netlify.app',
   integrations: [mdx({
     remarkPlugins: [remarkReadingTime]
-  }), sitemap(), tailwind(), icon(), compress()]
+  }), sitemap(), tailwind(), icon()]
 });
