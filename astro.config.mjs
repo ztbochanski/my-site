cca452e1bfa9401713721e6d940c10880ed93bb5
Zach Bochanski <ztbochanski@gmail.com>
2024-02-06 15:18:21 -0800

    remove preact edit spacing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
-import { defineConfig } from 'astro/config';
-import { remarkReadingTime } from './src/remark-reading-time.mjs';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import { remarkReadingTime } from "./src/remark-reading-time.mjs";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import preact from '@astrojs/preact';
 import serviceWorker from "astrojs-service-worker";
-
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://zachsblog.netlify.app',
-  integrations: [mdx({
-    remarkPlugins: [remarkReadingTime]
-  }), sitemap(), preact(), tailwind(), icon(), serviceWorker()]
+  site: "https://zachsblog.netlify.app",
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkReadingTime],
+    }),
+    sitemap(),
+    tailwind(),
+    icon(),
+    serviceWorker(),
+  ],
 });
